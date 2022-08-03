6186c3f07ed7b3a96eb5e830132e8f4eaee16fa1
pjmawuli <87507466+pjmawuli@users.noreply.github.com>
2022-08-03 10:41:53 +0000

    first coding assignment

---
+++ sum-of-primes.py
@@ -0,0 +1,18 @@
+# 10990259
+# Padi Julius Nartey Mawuli
+# I certify that I, Padi Julius Nartey Mawuli, wrote this code all by myself.
+
+number = int(input("Hello, type in a number 😃: "))
+sum = 0
+
+for x in range(1, number + 1):
+    factor_count = 0
+    for c in range(1, x + 1):
+        if x % c == 0:
+            factor_count += 1
+    if factor_count == 2:
+        sum += c
+        print(x)
+
+print(f"The sum of the prime numbers below {number} is {sum}.")
+
